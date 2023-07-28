8d3bb39656423cadcc4b3982363f43a36b40f560
rdhafa <rdhafa77@gmail.com>
2023-07-28 23:36:17 +0700

    first commit

--- routes/film.js
+++ routes/film.js
@@ -2,3 +2,8 @@ const pool = require("../config/config.js");
 const route = require("./route.js");
 
 // console.log(route);
+// route query all film
+route.get();
+
+// route film with id
+// route query film with category
